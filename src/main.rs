ecb4c0780cb0d275325b23d8afe0f81a6d53a2d9
Daniel Rahier <2395679+ifschleife@users.noreply.github.com>
2018-04-16 23:20:17 +0200

    Writing png file instead of just writing to stdout
    Introduced image crate which also allowed to remove custom color module.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 extern crate cgmath;
+extern crate image;
 mod color;
 mod ray;
 
-use cgmath::*;
 use std::f32;
 
-use color::{Rgb};
+use cgmath::*;
+
 use ray::{Ray};
 
 
-fn color(r: Ray) -> Rgb {
+fn color(r: Ray) -> Vector3<f32> {
     let direction = r.direction.normalize();
     let t = 0.5*(direction.y + 1.0);
-    let color = (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 
-    Rgb::from(color)
+    (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0)
 }
+
 
 fn main() {
     const NX: u32 = 200;
@@ -26,17 +27,21 @@ fn main() {
     let vertical = vec3(0.0, 2.0, 0.0);
     let origin = vec3(0.0, 0.0, 0.0);
 
-    println!("P3\n{} {}\n255\n", NX, NY);
+    let mut buffer = vec![0; (NX * NY * 3) as usize];
+
     for y in (0..NY).rev() {
         for x in 0..NX {
             let u = x as f32 / NX as f32;
             let v = y as f32 / NY as f32;
-            let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical);
-            let color = color(r);
-            let r = (255.99 * color.r) as u32;
-            let g = (255.99 * color.g) as u32;
-            let b = (255.99 * color.b) as u32;
-            println!("{} {} {}", r, g, b);
+            let ray = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical);
+            let color = color(ray);
+            
+            let index = (y*NX*3 + x*3) as usize;
+            buffer[index + 0] = (255.99 * color[0]) as u8;
+            buffer[index + 1] = (255.99 * color[1]) as u8;
+            buffer[index + 2] = (255.99 * color[2]) as u8;
         }
     }
+
+    image::save_buffer("image.png", &buffer, NX, NY, image::RGB(8)).unwrap()
 }
